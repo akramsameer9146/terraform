0b619bccd62501d68e0aa7f3350da65805391ca3
akramsameer9146 <akramsameer9146@gmail.com>
2026-03-05 03:37:49 +0530

    route igw in default rt and associate subnet in it

--- vpc_04-03-26/vpc.tf
+++ vpc_04-03-26/vpc.tf
@@ -13,6 +13,7 @@ resource "aws_vpc" "tf_vpc" {
 resource "aws_subnet" "tf_subnet" {
   vpc_id     = aws_vpc.tf_vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "my-subnet"
@@ -25,4 +26,21 @@ resource "aws_internet_gateway" "tf_internet_gateway" {
   tags = {
     Name = "my-internet-gateway"
   }
+}
+
+resource "aws_default_route_table" "tf_default_route_table" {
+  default_route_table_id = aws_vpc.tf_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.tf_internet_gateway.id
+  }
+tags = {
+  Name= "my-default-route-table"
+}
+}
+
+resource "aws_route_table_association" "tf_route_table_association" {
+  subnet_id      = aws_subnet.tf_subnet.id
+  route_table_id = aws_vpc.tf_vpc.default_route_table_id
 }
