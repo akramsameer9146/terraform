7ef5b4c3f83e5fc32b09caad150d47d3422789af
akramsameer9146 <akramsameer9146@gmail.com>
2026-03-05 02:24:37 +0530

    create subnet in the newly created vpc

--- vpc_04-03-26/vpc.tf
+++ vpc_04-03-26/vpc.tf
@@ -1,11 +1,20 @@
 provider "aws" {
   region = "ap-south-2"
 }
-resource "aws_vpc" "main" {
+resource "aws_vpc" "tf_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "tf_subnet" {
+  vpc_id     = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my-subnet"
   }
 }
